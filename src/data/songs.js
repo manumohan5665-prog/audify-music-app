7a20c80a19513c9e76b57b4eb61e744e0bc535d8
Manu Mohandas <manumohan5665@gmail.com>
2026-09-11 11:41:22 +0530

    added songs.js

---
+++ src/data/songs.js
@@ -0,0 +1,67 @@
+export const playlists = [
+    {
+        id: 1,
+        title: "Chill Vibes",
+        type: "Playlist",
+        image: "🌅",
+    },
+
+    {
+        id: 2,
+        title: "Focus Mode",
+        type: "Playlist",
+        image: "🌿",
+    },
+
+    {
+        id: 3,
+        title: "Workout",
+        type: "Playlist",
+        image: "🎧",
+    },
+
+    {
+        id: 4,
+        title: "Late Night",
+        type: "Playlist",
+        image: "🌙",
+    },
+];
+
+
+export const songs = [
+    {
+        id: 1,
+        title: "Blinding Lights",
+        artist: "The Weeknd",
+        image: "🎵",
+    },
+
+    {
+        id: 2,
+        title: "Night Changes",
+        artist: "One Direction",
+        image: "🎸",
+    },
+
+    {
+        id: 3,
+        title: "Snooze",
+        artist: "SZA",
+        image: "🌙",
+    },
+
+    {
+        id: 4,
+        title: "I Was Never There",
+        artist: "The Weeknd",
+        image: "🎧",
+    },
+
+    {
+        id: 5,
+        title: "Drive",
+        artist: "Harry Styles",
+        image: "🌅",
+    },
+];
